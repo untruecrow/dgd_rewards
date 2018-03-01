c8efea799ebacbfd40def5947b258e96c2462e18
thomas <northeyt@live.ie>
2018-03-01 14:47:56 +0000

    1st commit

---
+++ variables.py
@@ -0,0 +1,20 @@
+
+# Variables
+
+# dgd_owned = 10			# number of DGD owned
+percent_locked_up = 50			# percentage of DGD locked up for quarterly rewards
+percent_voting = 95			# percentage of locked up DGD that is active and voting
+n_dgx = 100*1e6				# expected total number of DGX tokens
+daily_dgx_transacted = 1 * 0.01 * n_dgx	# BTC has about 1% of its total supply transacted daily on-chain
+
+
+# Semi-fixed values
+gold_usd = 44			# USD value of 1 g of gold ( = 1 DGX token)
+
+
+# Fixed values
+n_dgd = 2e6			# total number of DGDs
+tx_fee = 0.13			# transaction fee percentage
+demurrage = 0.2			# percent demurrage that goes to DGD holders
+
+
